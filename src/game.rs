369b36c3190cfc456f9bbf0ab9bc74a1021911f9
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-12-20 19:00:03 +0000

    GameState: Flesh out the game state handling

--- src/game.rs
+++ src/game.rs
@@ -4,14 +4,15 @@ use rand::{thread_rng, Rng};
 
 const SPACE: char = 0x20 as char;
 const DEFAULT_BAG: [char; 100] = [
-    SPACE, SPACE,  'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D', 
-    'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 
+    SPACE, SPACE, 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D',
+    'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H',
     'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N',
     'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R',
     'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V',
-    'W', 'W', 'X', 'Y', 'Y', 'Z'
+    'W', 'W', 'X', 'Y', 'Y', 'Z',
 ];
 
+#[derive(Debug)]
 pub struct GameState {
     bag: Vec<char>,
     players: [Option<Player>; 4],
@@ -25,12 +26,14 @@ impl GameState {
         }
     }
 
-    fn initialize(&mut self) {
-        
-    }
+    fn initialize(&mut self) {}
 
     pub fn add_player(&mut self, player: Player) -> bool {
-        let mut empty_slots = self.players.iter_mut().enumerate().filter(|(_, s)| s.is_none());
+        let mut empty_slots = self
+            .players
+            .iter_mut()
+            .enumerate()
+            .filter(|(_, s)| s.is_none());
         if let Some((first_slot, _)) = empty_slots.next() {
             self.players[first_slot] = Some(player);
             true
@@ -40,7 +43,7 @@ impl GameState {
     }
 
     fn draw(&mut self, player: &mut Player) -> bool {
-        let index: usize = thread_rng().gen_range(0..self.bag.len()-1);
+        let index: usize = thread_rng().gen_range(0..self.bag.len() - 1);
         if let Some(character) = self.bag.iter_mut().nth(index) {
             player.insert_into_hand(*character)
         } else {
@@ -49,6 +52,7 @@ impl GameState {
     }
 }
 
+#[derive(Debug)]
 pub struct Player {
     username: &'static str,
     score: u32,
@@ -82,10 +86,15 @@ fn get_character_score(character: char) -> u32 {
         'K' => 5,
         'J' | 'X' => 8,
         'Q' | 'Z' => 10,
-        _ => 0
+        _ => 0,
     }
 }
 
 fn calculate_score_from_placement(characters: Vec<char>) -> u32 {
-    characters.iter().map(|c| get_character_score(*c) ).collect::<Vec<u32>>().iter().sum()
+    characters
+        .iter()
+        .map(|c| get_character_score(*c))
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum()
 }
