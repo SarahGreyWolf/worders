1c84055c761f39188c695224e5d3e35a204a80dd
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-12-22 01:16:58 +0000

    Web: Split connection into separate exposed function

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,10 @@ cfg_if::cfg_if! {
     if #[cfg(target_arch="wasm32")] {
         use wasm_bindgen::prelude::*;
         use web_sys::HtmlTableCellElement;
+        use web_sys::WebSocket;
+        use wasm_bindgen::JsCast;
         use util::*;
+        use table::Table;
     }
 }
 
@@ -24,10 +27,15 @@ cfg_if::cfg_if! {
 #[wasm_bindgen(start)]
 pub fn start() {
     console_error_panic_hook::set_once();
-    table::Table::new(15, 15, 2, None);
-    let mut game_state = GameState::new();
-    if !game_state.add_player(Player::new("SarahGreyWolf")) {
-        console_log!("Something went wrong!");
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub fn connect(hash: usize, id: usize) {
+    let mut table = Table::new(15, 15, 2, None);
+    let mut ws = WebSocket::new("ws://192.168.0.14:8080").unwrap();
+    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
+}
     }
     console_log!("{:?}", game_state);
 }
