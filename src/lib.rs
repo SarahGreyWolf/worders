47d00bf85d33c8d4df2d8cbb5d02d3d72497ba22
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-12-24 14:56:22 +0000

    Web: Convert to using new packet definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use game::{GameState, Player};
 
 pub mod game;
 pub mod packets;
@@ -12,6 +11,7 @@ cfg_if::cfg_if! {
         use wasm_bindgen::JsCast;
         use util::*;
         use table::Table;
+        use packets::Packets;
     }
 }
 
@@ -32,8 +32,8 @@ pub fn start() {
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
 pub fn connect(hash: usize, id: usize) {
-    let mut table = Table::new(15, 15, 2, None);
-    let mut ws = WebSocket::new("ws://192.168.0.14:8080").unwrap();
+    let mut table = Table::new(15, 15, 2, None, Some("board"), None);
+    let ws = WebSocket::new("ws://192.168.0.14:8080").unwrap();
     ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
     setup_closures(hash, id, ws, &mut table);
 }
@@ -45,23 +45,45 @@ fn setup_closures(hash: usize, id: usize, ws: WebSocket, table: &mut Table) {
     let ws_clone = ws.clone();
     let mut id = 0;
 
+    let test = packets::PlayerState::new(
+        0,
+        0,
+        "SarahGreyWolf".to_string(),
+        vec!['S', 'A', 'R', 'A', 'H', ' ', ' '],
+        4,
+    );
+    let _ws_clone = ws.clone();
     // On Connection Established
     let onopen = Closure::wrap(Box::new(move || {
+        let mut send_buffer = vec![];
+        let test_clone = test.clone();
+        test_clone.encode(&mut send_buffer).unwrap();
+        /*ws_clone
+        .send_with_u8_array(&send_buffer.as_slice())
+        .unwrap();*/
     }) as Box<dyn FnMut()>);
     ws.set_onopen(Some(onopen.as_ref().unchecked_ref()));
     onopen.forget();
-    //Cell clicked
+
+    // Cell clicked
     for cell in table.get_cells() {
         let ws_clone = ws.clone();
         let onclick = Box::new(move || {
-            let mut send_buffer = vec![];
-            let placement = packets::Place::new(id, 'S', cell.position[0], cell.position[1]);
-            placement.encode(&mut send_buffer);
-            if send_buffer.len() > 0 {
-                ws_clone
-                    .send_with_u8_array(&send_buffer.as_slice())
-                    .unwrap();
+            let mut character = ' ';
+            if cell.element.inner_text() != "" {
+                cell.element.set_inner_text("");
+            } else {
+                cell.element.set_inner_text("S");
+                character = 'S';
             }
+            let mut send_buffer = vec![];
+            let placement =
+                packets::Place::new(id as u16, character, cell.position[0], cell.position[1]);
+            Packets::Place(placement).encode(&mut send_buffer).unwrap();
+            assert!(send_buffer.len() > 0);
+            ws_clone
+                .send_with_u8_array(&send_buffer.as_slice())
+                .unwrap();
         }) as Box<dyn FnMut()>;
         table
             .get_cell(cell.position[0], cell.position[1])
